4993725450551a0f1c1b936f5e0751eb2c28a71f
Evgeniy Terekhin <e_terekhin@pm.me>
2024-12-01 13:17:22 +0200

    day 1

---
+++ bin/1/p1.rs
@@ -0,0 +1,34 @@
+static INPUT: &str = include_str!("../p1-input");
+// static INPUT: &str = include_str!("./p1-input-example");
+
+fn main() {
+    println!("SOLUTION: {}", solution(INPUT));
+}
+
+fn solution(input: &str) -> u64 {
+    let split = |line: &str| {
+        let mut parts = line.split_whitespace();
+        let l = parts.next().unwrap().parse::<u64>().unwrap();
+        let r = parts.next().unwrap().parse::<u64>().unwrap();
+
+        (l, r)
+    };
+
+    let (mut ls, mut rs): (Vec<_>, Vec<_>) = input.lines().map(split).unzip();
+    ls.sort_unstable();
+    rs.sort_unstable();
+
+    ls.into_iter().zip(rs).map(|(l, r)| l.abs_diff(r)).sum()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    static INPUT_EXAMPLE: &str = include_str!("../p1-input-example");
+
+    #[test]
+    fn test_example() {
+        assert_eq!(solution(INPUT_EXAMPLE), 11);
+    }
+}
---
+++ bin/1/p2.rs
@@ -0,0 +1,44 @@
+use std::collections::HashMap;
+
+static INPUT: &str = include_str!("../p1-input");
+// static INPUT: &str = include_str!("./p1-input-example");
+
+fn main() {
+    println!("SOLUTION: {}", solution(INPUT));
+}
+
+fn solution(input: &str) -> u64 {
+    let split = |line: &str| {
+        let mut parts = line.split_whitespace();
+        let l = parts.next().unwrap().parse::<u64>().unwrap();
+        let r = parts.next().unwrap().parse::<u64>().unwrap();
+
+        (l, r)
+    };
+
+    let (ls, rs): (Vec<_>, Vec<_>) = input.lines().map(split).unzip();
+
+    let rcounts = rs
+        .into_iter()
+        .fold(HashMap::<u64, u64>::new(), |mut acc, x| {
+            (*acc.entry(x).or_default()) += 1;
+
+            acc
+        });
+
+    ls.into_iter()
+        .map(|l| l * rcounts.get(&l).copied().unwrap_or_default())
+        .sum()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    static INPUT_EXAMPLE: &str = include_str!("../p1-input-example");
+
+    #[test]
+    fn test_example() {
+        assert_eq!(solution(INPUT_EXAMPLE), 31);
+    }
+}
