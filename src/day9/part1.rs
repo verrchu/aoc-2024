84d1e9d4db5372b6a200343a29bb8fa91972cc40
Evgeniy Terekhin <e_terekhin@pm.me>
2024-12-09 22:16:55 +0200

    day 9 part 1 rewrite

--- src/day9/part1.rs
+++ src/day9/part1.rs
@@ -1,43 +1,74 @@
-use std::cell::RefCell;
+use std::cmp::Ordering;
+use std::collections::{BTreeMap, VecDeque};
 
 pub fn solution(input: &str) -> usize {
-    let disk = input
-        .trim()
-        .chars()
-        .map(|ch| ch.to_digit(10).unwrap())
-        .enumerate()
-        .flat_map(|(i, n)| {
-            let elem = (i % 2 == 0)
-                .then(|| RefCell::new(Some(i / 2)))
-                .unwrap_or(RefCell::new(None));
+    let disk = input.trim().chars().map(|ch| ch.to_digit(10).unwrap());
 
-            (0..n).map(move |_| elem.clone())
-        })
-        .collect::<Vec<_>>();
+    let mut files = BTreeMap::<usize, (usize, u32)>::new();
+    let mut holes = VecDeque::<(usize, u32)>::new();
 
-    'outer: for (ri, rn) in disk.iter().enumerate().rev() {
-        if rn.borrow().is_none() {
-            continue 'outer;
+    let mut idx = 0;
+    for (i, n) in disk.enumerate() {
+        if i % 2 == 0 {
+            let file_id = files.len();
+            files.insert(idx, (file_id, n));
+        } else if n > 0 {
+            holes.push_back((idx, n));
         }
 
-        'inner: for (li, ln) in disk.iter().enumerate() {
-            if li >= ri {
-                break 'outer;
-            }
+        idx += n as usize;
+    }
 
-            if ln.borrow().is_some() {
-                continue 'inner;
-            }
+    loop {
+        let Some(mut rightmost_file) = files.last_entry() else {
+            break;
+        };
 
-            ln.swap(rn);
-            break 'inner;
+        let Some((hole_idx, hole_size)) = holes.front_mut() else {
+            break;
+        };
+
+        let file_idx = *rightmost_file.key();
+        let (file_id, file_size) = rightmost_file.get_mut();
+
+        if *hole_idx >= file_idx {
+            break;
+        }
+
+        match hole_size.cmp(&file_size) {
+            Ordering::Less => {
+                *file_size -= *hole_size;
+
+                let (file_id, _file_size) = (*file_id, *file_size);
+                files.insert(*hole_idx, (file_id, *hole_size));
+
+                holes.pop_front().unwrap();
+            }
+            Ordering::Equal => {
+                let (file_id, file_size) = (*file_id, *file_size);
+
+                files.pop_last().unwrap();
+                files.insert(*hole_idx, (file_id, file_size));
+
+                holes.pop_front().unwrap();
+            }
+            Ordering::Greater => {
+                let (file_id, file_size) = (*file_id, *file_size);
+
+                files.pop_last().unwrap();
+                files.insert(*hole_idx, (file_id, file_size));
+
+                *hole_idx += file_size as usize;
+                *hole_size -= file_size;
+            }
         }
     }
 
-    disk.into_iter()
-        .take_while(|n| n.borrow().is_some())
-        .map(|n| n.take().unwrap())
-        .enumerate()
-        .map(|(i, n)| i * n)
+    files
+        .into_iter()
+        .map(|(file_idx, (file_id, file_size))| {
+            let avg_idx = (file_idx as f64 + (file_idx + file_size as usize - 1) as f64) / 2.0;
+            (avg_idx * file_size as f64).round() as usize * file_id
+        })
         .sum()
 }
