c73a74db729971f0756757a6b8991024e02fd15a
Evgeniy Terekhin <e_terekhin@pm.me>
2024-12-09 21:09:20 +0200

    day 9 comments

--- src/day9/part2.rs
+++ src/day9/part2.rs
@@ -19,20 +19,28 @@ pub fn solution(input: &str) -> usize {
     }
 
     for (file_idx, _file_id, file_size) in files.iter_mut().rev() {
+        // find leftmost suitable hole
         let hole = holes
             .iter()
+            // filter out holes that are too small
             .filter(|(hole_size, _hole_idxs)| *hole_size >= file_size)
+            // pick existing leftmost holes for all suitable sizes
             .filter_map(|(hole_size, hole_idxs)| {
                 hole_idxs.first().map(|hole_idx| (hole_size, hole_idx))
             })
+            // filter out holes that are to the right of the file
             .filter(|(_hole_size, hole_idx)| *hole_idx < file_idx)
+            // get leftmost hole
             .min_by_key(|(_hole_size, hole_idx)| *hole_idx)
+            // break references to holes mapping
             .map(|(hole_size, hole_idx)| (*hole_size, *hole_idx));
 
         if let Some((hole_size, hole_idx)) = hole {
+            // if hole is found remove it from holes mapping
             holes.get_mut(&hole_size).unwrap().pop_first().unwrap();
 
             *file_idx = hole_idx;
+            // if hole is bigger than the file add a leftover hole back into the mapping
             if hole_size > *file_size {
                 let size_diff = hole_size - *file_size;
                 holes
